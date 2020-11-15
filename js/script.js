082c3772e173ced88e9531f80a1f3b1e8793cdcd
Amane Hosanna <amanehosanna@gmail.com>
2020-11-15 01:50:07 +0000

    added floating action button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
 
-// show navbar on scroll
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    console.log("v");
-    // var currentScrollPos = window.pageYOffset;
-    if (window.pageYOffset < document.getElementById("header").style.height) {
-        document.getElementById("navbar").style.top = "-50px";
-        document.getElementById("navbar").style.opacity = 0;
-    } else {
-        document.getElementById("navbar").style.top = "0px";
-        document.getElementById("navbar").style.opacity = 1;
-        
-    }
+console.log("script");
+function toggleFloatingButton() {
+    var isShowcaseInView = Utils.isElementInView($("#showcase"), false);
+    var isFooterInView = Utils.isElementInView($("#footer"), false);
+    var book_btn = document.getElementById("book_now");
+
     
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-};
+    if (isFooterInView) {
+        if (!book_btn.classList.contains("remove")) {
+            book_btn.classList.add("remove");
+        }
+        return;
+    } 
+
+    if (isShowcaseInView) {
+        if (!book_btn.classList.contains("remove")) {
+            book_btn.classList.add("remove");
+        }
+        return;
+    }
+
+    if (book_btn.classList.contains("remove")) {
+        book_btn.classList.remove("remove");
+    }
+}
+
+
+function isElementInViewport(el) {
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */ &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+setInterval(() => {
+    toggleFloatingButton();
+}, 500);
---
+++ js/utils.js
@@ -0,0 +1,19 @@
+function Utils() {}
+
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return pageTop < elementTop && pageBottom > elementBottom;
+        } else {
+            return elementTop <= pageBottom && elementBottom >= pageTop;
+        }
+    },
+};
+
+var Utils = new Utils();
